de2941236dd9d075e051f945a04843701f322954
caitui <jon.zhu@lixinchuxing.com>
2020-01-16 15:39:20 +0800

    [ADD]nightswatch插件

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-Nightswatch
+# Sentry-NightsWatch
 
-> 定制版sentry pagerduty插件,支持自定义服务器地址
+`Sentry Server`对接`NightsWatch`插件
 
-初始代码来自[Sentry plugin](https://github.com/getsentry/sentry/tree/master/src/sentry_plugins/pagerduty)
+## 安装
+
+```bash
+$ pip install sentry-nightswatch
+```
+
+## 使用
+
+在`项目`的所有集成页面找到`NightsWatch`插件，启用，并设置`Service Key`
+
+![plugin](https://gitlab.lixinio.com/sre/sentry-nightswatch/raw/master/docs/images/options.png)
+
+在插件上使用`Test Plugin`进行测试，当配置好`Service Key`后，会有如下显示:
+
+![plugin](https://gitlab.lixinio.com/sre/sentry-nightswatch/raw/master/docs/images/test.png)
--- src/nightswatch/plugin.py
+++ src/nightswatch/plugin.py
@@ -8,27 +8,36 @@ from sentry.utils.http import absolute_uri
 from sentry_plugins.base import CorePluginMixin
 from sentry_plugins.utils import get_secret_field_config
 
-from .client import PagerDutyClient
+from .client import NightsWatchClient
 
 
-class PagerDutyPlugin(CorePluginMixin, NotifyPlugin):
-    description = "Send alerts to PagerDuty."
-    slug = "pagerduty"
-    title = "PagerDuty"
+class NightsWatchPlugin(CorePluginMixin, NotifyPlugin):
+    description = "Send alerts to NightsWatch."
+    slug = "nightswatch"
+    title = "NightsWatch"
     conf_key = slug
     conf_title = title
 
     def is_configured(self, project, **kwargs):
-        return bool(self.get_option("service_key", project))
+        return bool(self.get_option("service_key", project)) and \
+            bool(self.get_option("api_url", project))
 
     def get_config(self, **kwargs):
         service_key = self.get_option("service_key", kwargs["project"])
         secret_field = get_secret_field_config(
-            service_key, "PagerDuty's Sentry service Integration Key", include_prefix=True
+            service_key, "NightsWatch's Sentry Service Integration Key", include_prefix=True
         )
         secret_field.update({"name": "service_key", "label": "Service Key"})
+
+        api_url = self.get_option("api_url", kwargs["project"])
+        url_field = get_secret_field_config(
+            api_url, "NightsWatch's Sentry Integration API URL", include_prefix=True
+        )
+        url_field.update({"name": "api_url", "label": "API URL"})
+
         return [
             secret_field,
+            url_field,
             {
                 "name": "routes",
                 "label": "Tag routing",
@@ -36,7 +45,7 @@ class PagerDutyPlugin(CorePluginMixin, NotifyPlugin):
                 "placeholder": "environment,production,KEY1\ndevice,Other,KEY2",
                 "required": False,
                 "help": (
-                    "Route each event to a different PagerDuty service key based "
+                    "Route each event to a different NightsWatch service key based "
                     "on the event's tags. Each line should contain a tag, "
                     "value, and service key separated by commas. The first "
                     "line that matches a tag's key and value will send to that "
@@ -49,24 +58,23 @@ class PagerDutyPlugin(CorePluginMixin, NotifyPlugin):
         if not self.is_configured(group.project):
             return
 
-        # TODO: This should eventually be event.title in line with other plugins.
-        # However, we should notify users first, since PD alert routing may be
-        # based off the message field.
-        description = (event.real_message or event.message)[:1024]
+        description = event.get_legacy_message()[:1024]
 
-        tags = dict(event.tags)
+        tags = dict(event.get_tags())
         details = {
             "event_id": event.event_id,
             "project": group.project.name,
+            "environment": event.get_tag("environment"),
             "release": event.get_tag("sentry:release"),
             "platform": event.platform,
             "culprit": event.culprit,
             "datetime": event.datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
             "tags": tags,
-            "url": group.get_absolute_url(params={"referrer": "pagerduty_plugin"}),
+            "url": group.get_absolute_url(params={"referrer": "nightswatch_plugin"}),
         }
 
         service_key = self.get_option("service_key", group.project)
+        api_url = self.get_option("api_url", group.project)
 
         routes = self.get_option("routes", group.project) or ""
         for route in (r.strip() for r in routes.split("\n")):
@@ -78,7 +86,7 @@ class PagerDutyPlugin(CorePluginMixin, NotifyPlugin):
                 service_key = route_service_key
                 break
 
-        client = PagerDutyClient(service_key=service_key)
+        client = NightsWatchClient(service_key=service_key, api_url=api_url)
         try:
             response = client.trigger_incident(
                 description=description,
@@ -98,3 +106,4 @@ class PagerDutyPlugin(CorePluginMixin, NotifyPlugin):
             assert response["status"] == "success"
         except Exception as e:
             self.raise_error(e)
+
